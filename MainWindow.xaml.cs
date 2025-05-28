d4bd17a8416f64db7390db159adb44fe964f5bb5
ghhenrisar <81454067+ghhenrisar@users.noreply.github.com>
2025-05-28 21:48:01 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CustomGadgetApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ClockText.Text = DateTime.Now.ToString("HH:mm:ss");
+            WeatherText.Text = "Weather: Sunny, 25°C"; // Placeholder
+        }
+    }
+}
